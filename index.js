c730f253d27ee9c9ac3d43e8402f06c0e6aa060c
marchefk <jackiewicz.ewa@wp.pl>
2017-07-12 23:03:55 +0100

    Started working on navbar

---
+++ index.js
@@ -0,0 +1,20 @@
+let navbar = document.getElementsByClassName('nav');
+navbar = [].slice.call(navbar);
+let icons = {
+  'home': '<i class="fa fa-home" aria-hidden="true"></i>',
+  'about': '<i class="fa fa-user-circle-o" aria-hidden="true"></i>',
+  'projects': '<i class="fa fa-code" aria-hidden="true"></i>',
+  'contact': '<i class="fa fa-envelope-open-o" aria-hidden="true"></i>'
+}
+
+navbar.forEach(function(e){
+  console.log(e);
+  e.onMouseOver = function() {
+    for(var i in icons){
+      console.log(i);
+      if(e.hasClassName(i)){
+        e.innerHTML = icons[i];
+      }
+    }
+  }
+});
